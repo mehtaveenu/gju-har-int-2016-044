6a1a86017e5010cacc94a6bbfc38de0b6f08e5bf
mehtaveenu <narangveenu0007@gmail.com>
2017-01-20 13:48:02 +0530

    Add files via upload

---
+++ veenu_14012044/README.md
@@ -0,0 +1,2 @@
+# gju-har-int-2016-047
+# gju-har-int-2016-047
---
+++ veenu_14012044/mean-project/app/frontend/js/controllers/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('todo')
+  .controller('MainCtrl', function ($rootScope) {
+	$rootScope.pageTitle = 'Homepage';
+  });
